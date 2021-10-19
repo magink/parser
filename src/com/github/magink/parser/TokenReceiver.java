e88a35f00a89f0e30004d4118f64d7f0b29c3611
magink <magink1991@gmail.com>
2021-10-19 18:17:06 +0200

    docs: interface to tokenizer

--- src/com/github/magink/parser/TokenReceiver.java
+++ src/com/github/magink/parser/TokenReceiver.java
@@ -1,13 +1,27 @@
 package com.github.magink.parser;
 
-import java.util.List;
-
 /**
  * The boundary towards the tokenizer/lexer.
  */
 public interface TokenReceiver {
+
+  /**
+   * Step to next token.
+   */
   public void next();
+  
+  /**
+   * @return true if there is a next token
+   */
   public boolean hasNext();
+
+  /**
+   * @return the type of the current token
+   */
   public String getType();
+
+  /**
+   * @return the value of the current token
+   */
   public String getValue();
 }
