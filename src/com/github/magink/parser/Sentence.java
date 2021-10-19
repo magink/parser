ee44e5de747f362fd8c85e27a1b1af1a506e5883
magink <magink1991@gmail.com>
2021-10-19 18:18:26 +0200

    feat: sentence class have words to get

--- src/com/github/magink/parser/Sentence.java
+++ src/com/github/magink/parser/Sentence.java
@@ -3,6 +3,7 @@ package com.github.magink.parser;
 import java.util.ArrayList;
 
 public class Sentence {
+  private int current = 0;
   private ArrayList<Word> words;
   private EndType endType;
   
@@ -11,6 +12,23 @@ public class Sentence {
   }
   public String getEndType() {
     return endType.getEndType();
+  }
+  public void nextWord(){
+    current = (current < words.size()) ? current + 1: words.size();
+  }
+  public void previousWord() {
+    current = (current > 0 ) ? current - 1 : 0;
+  }
+  public Word getWord(){
+    return words.get(current);
+  }
+  public String getWords() {
+    StringBuilder sentence = new StringBuilder();
+    words.forEach(word -> {
+      sentence.append(word.getReadableWord() + " ");
+    });
+    sentence.append(endType.getEndType());
+    return sentence.toString();
   }
   
 }
