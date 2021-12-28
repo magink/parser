23db6af7a09cdf98d7191006d3fce1f2b00e1c87
magink <magink1991@gmail.com>
2021-12-28 20:08:05 +0100

    test: tests for the parser

--- test/com/github/magink/parser/TestParser.java
+++ test/com/github/magink/parser/TestParser.java
@@ -1,8 +1,8 @@
 package com.github.magink.parser;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,16 +10,132 @@ public class TestParser {
   TokenReceiver tokenizer;
   Parser parser;
 
-  @BeforeEach
-  void setup() {
-    tokenizer = new TokenizerAdapter("This is a normal sentence.");
-    parser = new Parser(tokenizer);
-  }
   @Test
-  @DisplayName("Just something to start with.")
-  void testParser() {
+  @DisplayName("Correct word is returned from statement.")
+  void shouldReturnCorrectWordFromStatement() {
+    String input = "I love parsers.";
+    String expectedWord = "love";
+
+    tokenizer = new TokenizerAdapter(input);
+    parser = new Parser(tokenizer);
+
     Document doc = parser.parse();
-    assertEquals(doc.value, "Just something to start with.");
+    Sentences sentences = doc.getSentences();
+    for(Sentence s : sentences) {
+      s.nextWord();
+      s.nextWord();
+      s.previousWord();
+      assertEquals(expectedWord, s.getWord().getReadableWord());
+    }
   }
-  
+
+  @Test
+  @DisplayName("Two statements are returned.")
+  void shouldReturnTwoStatements() {
+    String input = "I love parsers. They are fun.";
+    int expectedNumberOfStatements = 2;
+
+    tokenizer = new TokenizerAdapter(input);
+    parser = new Parser(tokenizer);
+
+    Document doc = parser.parse();
+    Sentences sentences = doc.getSentences();
+    int countedStatements = 0;
+    for(Sentence s : sentences) {
+      countedStatements++;
+    }
+    assertEquals(expectedNumberOfStatements, countedStatements);
+  }
+
+  @Test
+  @DisplayName("Exclamation sentence is returned.")
+  void shouldReturnExclamation() {
+    String input = "I love parsers!";
+    tokenizer = new TokenizerAdapter(input);
+    parser = new Parser(tokenizer);
+    Document doc = parser.parse();
+    Sentences sentences = doc.getSentences();
+    for(Sentence s : sentences) {
+      s.getEndType().equals(Exclamation.TYPE);
+    }
+  }
+
+  @Test
+  @DisplayName("Question sentence is returned")
+  void shouldReturnQuestion() {
+    String input = "Do I love parsers?";
+    
+    tokenizer = new TokenizerAdapter(input);
+    parser = new Parser(tokenizer);
+    Document doc = parser.parse();
+    Sentences sentences = doc.getSentences();
+    for(Sentence s : sentences) {
+      s.getEndType().equals(Question.TYPE);
+    }
+  }
+
+  @Test
+  @DisplayName("All question sentences are returned")
+  void shouldReturnAllQuestions() {
+    String input = "One question? Then another questions? But not this one. A final question?";
+    int expectedNumberOfQuestions = 3;
+    
+    tokenizer = new TokenizerAdapter(input);
+    parser = new Parser(tokenizer);
+    Document doc = parser.parse();
+    Sentences questionSentences = doc.getQuestions();
+    int count = 0;
+    for(Sentence s : questionSentences) {
+      count++;
+    }
+    assertEquals(expectedNumberOfQuestions, count);
+  }
+
+  @Test
+  @DisplayName("All statement sentences are returned")
+  void shouldReturnAllStatements() {
+    String input = "This is a statement. No this is! Is it? Yes.";
+    int numberOfStatements = 2;
+
+    tokenizer = new TokenizerAdapter(input);
+    parser = new Parser(tokenizer);
+    Document doc = parser.parse();
+    Sentences statementSentences = doc.getStatements();
+    int count = 0;
+    for(Sentence s : statementSentences) {
+      count++;
+    }
+    assertEquals(numberOfStatements, count);
+  }
+
+  @Test
+  @DisplayName("All exclamations are returned")
+  void shouldReturnAllExclamations() {
+    String input = "Bratan now is the time! Go away necktie. I only ever wanted you to have fun Harry! How did we meet?";
+    int numberOfExclamations = 2;
+
+    tokenizer = new TokenizerAdapter(input);
+    parser = new Parser(tokenizer);
+    Document doc = parser.parse();
+    Sentences exclamationSentences = doc.getExclamations();
+    int count = 0;
+    for(Sentence s : exclamationSentences) {
+      count++;
+    }
+    assertEquals(numberOfExclamations, count);
+  }
+
+  @Test
+  @DisplayName("Entire sentence is returned with end symbol")
+  void shouldReturnEntireSentence() {
+    String input = "Bratan now is the time!";
+
+    tokenizer = new TokenizerAdapter(input);
+    parser = new Parser(tokenizer);
+    Document doc = parser.parse();
+    Sentences exclamationSentences = doc.getSentences();
+    for(Sentence s : exclamationSentences) {
+      assertEquals(input, s.toString());
+    }
+  }
 }
