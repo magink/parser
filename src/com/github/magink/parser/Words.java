51e45aae8a9e22ce371cd19262e8ae09c8c9e83f
magink <magink1991@gmail.com>
2021-12-28 20:07:18 +0100

    refactor: datastructure for words

---
+++ src/com/github/magink/parser/Words.java
@@ -0,0 +1,28 @@
+package com.github.magink.parser;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Words implements Iterable<Word> {
+  private ArrayList<Word> wordArray;
+  protected Words(){
+    wordArray = new ArrayList<>();
+  }
+  protected void add(Word word){
+    wordArray.add(word);
+  }
+  protected Word get(int index) {
+    return wordArray.get(index);
+  }
+  protected int size() {
+    return wordArray.size();
+  }
+  @Override
+  public Iterator<Word> iterator() {
+    return wordArray.iterator();
+  }
+  @Override
+  public String toString(){
+    return wordArray.toString();
+  }
+}
