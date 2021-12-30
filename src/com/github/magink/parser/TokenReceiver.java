179922f0bfda9aa27c34eac807a3449f76e8fa89
magink <magink1991@gmail.com>
2021-12-30 11:24:43 +0100

    Some documentation on the interface

--- src/com/github/magink/parser/TokenReceiver.java
+++ src/com/github/magink/parser/TokenReceiver.java
@@ -9,11 +9,13 @@ public interface TokenReceiver {
 
   /**
    * Step to next token.
+   * @return true if next token could be stepped to, false if not.
    */
   public boolean next();
 
   /**
-   * @return the current token
+   * Gets the current selected token from the tokenizer. 
+   * @return the selected token
    */
   public Token getToken();
 
