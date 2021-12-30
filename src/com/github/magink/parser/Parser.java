9ac84282c5240fe4d22d6597abfdb4400da0d29e
magink <magink1991@gmail.com>
2021-12-30 11:23:59 +0100

    refactor: Parser parse, not Document

--- src/com/github/magink/parser/Parser.java
+++ src/com/github/magink/parser/Parser.java
@@ -2,23 +2,63 @@ package com.github.magink.parser;
 
 import com.github.magink.tokenizer.Token;
 
+
 public class Parser {
 
   private Document document;
-  TokenReceiver tokenizer;
+  private TokenReceiver tokenizer;
+  private Words tempWords;
+  private Sentence sentence; 
 
   public Parser(TokenReceiver tokenizer) {
     this.tokenizer = tokenizer;
     document = new Document();
+    tempWords = new Words();
   }
 
   public Document parse() {
     do {
       Token token = tokenizer.getToken();
-      document.parse(token);     
+      parse(token);     
     } while (tokenizer.next());
     return document;
   }
- 
-  
+
+  protected void parse(Token token) {
+    if(isWord(token)) {
+      Word word = new Word(token.getValue());
+      tempWords.add(word);
+      return;
+    }
+    else if (isQuestion(token)) {
+      sentence = new Question();
+    }
+    else if(isStatement(token)) {
+      sentence = new Statement();
+    }
+    else if(isExclamation(token)) {
+      sentence = new Exclamation();
+    } else {
+      throw new SyntaxException(token.getType() + " couldn't be parsed");
+    }
+    sentence.addWords(tempWords);
+    tempWords = new Words();
+    document.addSentence(sentence);
+  }
+
+  private boolean isWord(Token token) {
+    return token.getType().equals(Word.TYPE);
+  }
+
+  private boolean isQuestion(Token token) {
+    return token.getType().equals(Question.TYPE);
+  }
+
+  private boolean isStatement(Token token) {
+    return token.getType().equals(Statement.TYPE);
+  }
+
+  private boolean isExclamation(Token token) {
+    return token.getType().equals(Exclamation.TYPE);
+  }
 }
