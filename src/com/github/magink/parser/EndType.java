ebac2013972813b8bc97a9567301ab0cd04737a1
magink <magink1991@gmail.com>
2021-12-30 11:24:55 +0100

    docs: documentation on endType

--- src/com/github/magink/parser/EndType.java
+++ src/com/github/magink/parser/EndType.java
@@ -1,7 +1,23 @@
 package com.github.magink.parser;
 
+/**
+ * All types of sentences must implement this interface for a valid sentence must have a type.
+ */
 public interface EndType {
+  /**
+   * Return the name of this type.
+   * @return The name of this type, e.g. "QUESTION".
+   */
   String getEndType();
+  /**
+   * Return the character symbol for this type
+   * @return the character, e.g. "?"
+   */
   String getSymbol();
+  /**
+   * This method returns the entire sentence with end type as a String.
+   * This method should call the super method of the base class Sentence first, and then add it's own end symbol before returning
+   * @return The complete sentence e.g. "Is this a question?"
+   */
   String toString();
 }
